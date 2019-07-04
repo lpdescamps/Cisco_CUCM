e7988770bca60caaeae1b78f3b7f96755db6e776
Louis-Philippe Descamps <lp.descamps@gmail.com>
2019-07-04 15:35:43 +0100

    Adding create udp by DN script

---
+++ UDP/CreateUDP_by_DN.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+
+from zeep import Client
+from zeep.cache import SqliteCache
+from zeep.transports import Transport
+from requests import Session
+from requests.auth import HTTPBasicAuth
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+from cryptography.fernet import Fernet
+from pathlib import Path
+from collections import OrderedDict
+from zeep.helpers import serialize_object
+
+REGION = 'Whatever region'
+UDP = '%'
+RANGEDN = '\+32'
+PREFIX = '817'
+SITE = 'EIBEBRU'
+NEWMODEL = 'Cisco 8845'
+PTN = 'phone button template with 2 URI'
+
+def file(path, platform, role):
+    username = path / REGION / platform / ('user_' + role + '.txt')
+    keyhash = path / REGION / platform / ('key_' + role + '.txt')
+    hash = path / REGION / platform / ('hash_' + role + '.txt')
+
+    return username, keyhash, hash
+def crypto(keyhash, hash):
+    with open(keyhash, 'rb') as file_key:
+        for line_key in file_key:
+            key = line_key
+            cipher_suite = Fernet(key)
+            with open(hash, 'rb') as file_hash:
+                for line_hash in file_hash:
+                    encryptedpwd = line_hash
+                    uncipher_text = (cipher_suite.decrypt(encryptedpwd))
+                    pwd = bytes(uncipher_text).decode("utf-8")
+                    return pwd
+def read(file):
+    datalist = []
+    for line in open(file):
+        data = line.strip('\n')
+        datalist.append(data)
+    return datalist
+def listUDP(client, udp):
+    return client.listDeviceProfile(**{
+        'searchCriteria': {
+            'name': udp,
+        },
+        'returnedTags': {
+            'name': ''
+        }
+    })
+def getUDP(client, udp):
+    return client.getDeviceProfile(name=udp)
+def getLine(client, dn, dnpt):
+    return client.getLine(pattern=dn,routePartitionName=dnpt)
+def getUser(client, user):
+    return client.getUser(userid=user)
+def sql_get_user_items(service, udp, dn):
+    sql_statement = \
+        "SELECT " \
+            "E.firstname, E.lastname, E.userid " \
+        "FROM " \
+            "enduserdevicemap EDM " \
+        "JOIN device D " \
+            "ON EDM.fkdevice = D.pkid " \
+        "JOIN enduser E " \
+            "ON EDM.fkenduser = E.pkid " \
+        "WHERE D.name = '"+udp+"'"
+    axl_resp = service.executeSQLQuery(sql=sql_statement)
+    table = [OrderedDict((element.tag, element.text) for element in row) for row in
+            serialize_object(axl_resp)["return"]["row"]]
+    firstname = [items['firstname'] for items in table][0]
+    lastname = [items['lastname'] for items in table][0]
+    userid = [items['userid'] for items in table][0]
+
+    newudp = userid + '_' + str(NEWMODEL[-4:]) + '-udp'
+    desc = SITE + '_' + firstname + ' ' + lastname + '_' + PREFIX + str(dn[-4:])
+    ldisplay = firstname + ' ' + lastname
+    llabel = firstname + ' ' + lastname + ' - ' + PREFIX + str(dn[-4:])
+
+    return newudp, desc, ldisplay, llabel, userid
+def addUDP(client, name, model, desc, ptn, local, dirn, dnpt, llabel, ldisplay):
+    client.addDeviceProfile(deviceProfile=
+        {
+        'name': name,
+        'product': model,
+        'description': desc,
+        'class': 'Device Profile',
+        'protocol': 'SIP',
+        'protocolSide': 'User',
+        'phoneTemplateName': ptn,
+        'userLocale': local,
+        'softkeyTemplateName': 'QBE Standard User',
+        'services': {
+            'service':
+                [
+                    {
+                        'telecasterServiceName': 'Service Desk',
+                        'name': 'Service Desk',
+                        'urlButtonIndex': '1',
+                        'urlLabel': 'Service Desk',
+                        'serviceNameAscii': 'Service Desk',
+                    },
+                    {
+                        'telecasterServiceName': 'Extension Mobility Logout',
+                        'name': 'Extension Mobility Logout',
+                        'urlButtonIndex': '2',
+                        'urlLabel': 'Extension Mobility Logout',
+                        'serviceNameAscii': 'Extension Mobility Logout',
+                    }
+                ],
+        },
+        'lines': {
+            'line': [
+                {
+                    'index': 1,
+                    'label': llabel,
+                    'display': ldisplay,
+                    'displayAscii': ldisplay,
+                    'dirn': {
+                        'pattern': dirn,
+                        'routePartitionName': {
+                            '_value_1': dnpt,
+                        },
+                    },
+                }
+            ],
+        },
+    })
+def updateLine(client, dn, pt, desc, ldisplay):
+    return client.updateLine(**{
+    'pattern': dn,
+    'routePartitionName': pt,
+    'description': desc,
+    'alertingName': ldisplay,
+    'asciiAlertingName': ldisplay
+    })
+def updateUser(client, user, *args):
+    return client.updateUser(**{
+    'userid': user,
+    'phoneProfiles': {
+        'profileName':
+            args[0]
+        }
+    })
+
+def main():
+    path = Path('C:\\shared\\API\\credentials')
+    wsdl = 'file://C://shared//API//axlsqltoolkit//schema//11.5//AXLAPI.wsdl'
+    platform = 'CUCM'
+    role = 'rwx'
+    urllib3.disable_warnings(InsecureRequestWarning)
+    server = path / REGION / platform / ('fqdn' + '.txt')
+    binding_name = '{http://www.cisco.com/AXLAPIService/}AXLAPIBinding'
+    address = 'https://{fqdn}:8443/axl/'.format(fqdn=read(server)[0])
+    session = Session()
+    session.verify = False
+    session.auth = HTTPBasicAuth(read(file(path, platform, role)[0])[0], crypto(file(path, platform, role)[1], file(path, platform, role)[2]))
+    transport = Transport(cache=SqliteCache(), session=session, timeout=60)
+    client = Client(wsdl=wsdl, transport=transport)
+    axl = client.create_service(binding_name, address)
+
+    for udps in listUDP(axl, UDP)['return']['deviceProfile']:
+        udp = getUDP(axl, udps)['return']['deviceProfile']['name']
+        if getUDP(axl, udps)['return']['deviceProfile']['lines'] is None:
+            print(udp, 'has no Directory Number')
+            print('#####The End#########The End#########The End#########The End####')
+            pass
+        else:
+            dn = [items['dirn']['pattern'] for items in getUDP(axl, udps)['return']['deviceProfile']['lines']['line']][0]
+            dnpt = [items['dirn']['routePartitionName']['_value_1'] for items in getUDP(axl, udps)['return']['deviceProfile']['lines']['line']][0]
+            local = getUDP(axl, udps)['return']['deviceProfile']['userLocale']
+
+            if dn.startswith(RANGEDN) is True:
+                udps = [items for items in getLine(axl,dn,dnpt)['return']['line']['associatedDevices']['device']]
+                models = [getUDP(axl, items)['return']['deviceProfile']['model'] for items in udps]
+                modeltuple = tuple(zip(models, udps))
+                newmodelcheck = [items for items in modeltuple if NEWMODEL in items]
+
+                for items in newmodelcheck:
+                    print('UDP ' + items[1] + ' on directory number ' + dn + ' already exist for model ' + NEWMODEL)
+                    print('#####The End#########The End#########The End#########The End####')
+
+                if not newmodelcheck:
+                    try:
+                        newudp = sql_get_user_items(axl, udp, dn)[0]
+                        desc = sql_get_user_items(axl, udp, dn)[1]
+                        ldisplay = sql_get_user_items(axl, udp, dn)[2]
+                        llabel = sql_get_user_items(axl, udp, dn)[3]
+                        userid = sql_get_user_items(axl, udp, dn)[4]
+                        udplist = [items['_value_1'] for items in getUser(axl, userid)['return']['user']['phoneProfiles']['profileName']]
+                        print("Cool! I found an enduser! Let's create the UDP: " + newudp + ' with description: ' + desc)
+                        addUDP(axl, newudp, NEWMODEL, desc, PTN, local, dn, dnpt, llabel, ldisplay)
+                        updateLine(axl, dn, dnpt, desc, ldisplay)
+                        udplist.append(newudp)
+                        updateUser(axl, userid, udplist)
+                        print('#####The End#########The End#########The End#########The End####')
+                    except:
+                        newudp = PREFIX + str(dn[-4:]) + '_' + str(NEWMODEL[-4:]) + '-udp'
+                        desc = 'FREE ' + SITE + '_firstname lastname_' + PREFIX + str(dn[-4:])
+                        ldisplay = 'firstname lastname'
+                        llabel = 'firstname lastname - ' + PREFIX + str(dn[-4:])
+                        print("Looks like a free UDP to me! Didnt find an enduser! Let's create the FREE UDP: " + newudp + ' with description: ' + desc)
+                        addUDP(axl, newudp, NEWMODEL, desc, PTN, local, dn, dnpt, llabel, ldisplay)
+                        updateLine(axl, dn, dnpt, desc, ldisplay)
+                        print('#####The End#########The End#########The End#########The End####')
+
+            else:
+                pass
+
+if __name__ == '__main__':
+    main()
--- UDP/README.md
+++ UDP/README.md
@@ -8,7 +8,7 @@ You will have to create credentials using Create_Login from [Login Credential](h
 > encrypted credential for CUCM using Create_Login script  
 
 ## UDP_to_User.py
-That script will scan the cucm and check if end user has a udp or not. If not, the script will check if a udp name matches the user ID. If it does it will return information. This script only reads the cucm, it won't write anything
+This script will scan the cucm and check if end user has a udp or not. If not, the script will check if a udp name matches the user ID. If it does it will return information. This script only reads the cucm, it won't write anything
 >Details explaining each variables
 * **REGION**: Where is your cluster. See the README.md for Create_Login  
 
@@ -17,3 +17,25 @@ under def main()
 * **wsdl**: The path where your wsdl is stored.
 * **platform**: The platform we are using. I assume it would be CUCM. See the README.md for Create_Login
 * **role**: To define the API user's permission. Here we need read permission so r. See the README.md for Create_Login
+
+## CreateUDP_by_DN
+This script will scan the cucm based on a DN range. It will 
+ - create a new UDP based on the requiered model.
+ - Pull information from End user to build the new UDP. If no enduser, it will create a orphean UDP.
+ - If end user exists, it will add the udp to the end user BUT won't remove the old UDP.
+The UDP will have 2 uri but you can amend this under def addUDP
+ 
+>Details explaining each variables
+* **REGION**: Where is your cluster. See the README.md for Create_Login  
+* **UDP**: You can narrow down to a specific User Device Profile or use the wildcard %
+* **RANGEDN**: Need to put the beginning of the DN range. For example, '\+32' but you can put the full DN
+* **PREFIX**: Sometime your full DN has an extension. For example +32123456789 has extension '8176789'. The prefix would be '817'
+* **SITE**: It will be used for line and UDP description. For example EUBEBRU would be my site code (EU: Europe, BE: Belgium, BRU: Brussels) and my description would be EUBEBRU - LouisP Descamps - 8176789
+* **NEWMODEL**: 'Cisco 8845' Needs to match what you have in CUCM
+* **PTN**: Your phone button template
+
+under def main()
+* **path**: The path where your credentials are stored. See the README.md for Create_Login
+* **wsdl**: The path where your wsdl is stored.
+* **platform**: The platform we are using. I assume it would be CUCM. See the README.md for Create_Login
+* **role**: To define the API user's permission. Here we need full permission so rwx. See the README.md for Create_Login
